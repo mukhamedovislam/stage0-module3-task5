4f10d9cdbf2ada5fe32a18242b8625d0c0e62f4f
mukhamedovislam <bozuy24@gmail.com>
2022-09-08 12:09:10 +0400

    Fourth Submit

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -3,7 +3,7 @@ package lang.print.gaps.task5;
 public class NumberDivider {
     void divide(int toBeDivided) {
     
-	float result = toBeDivided / 5.0;
+	double result = toBeDivided / 5.0;
 	System.out.println(result);
 
 
